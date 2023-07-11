970bc8cea4eab30d2ff67c80742b889d6ff43771
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-07-11 17:59:52 +0100

    Create main form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Form from "./components/Form";
 import "./styles.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Form />
     </div>
   );
 }
