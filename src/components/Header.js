bd595cbf5188b0f787c2aa4bc0d7b4d247e054bf
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-07-11 17:38:03 +0100

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className='header'>
+      <img src='/img/troll-face.png' alt='meme face' className='header-image'></img>
+      <h1 className='header-title'>Meme Generator</h1>
+    </div>
+  );
+}
+
+export default Header
